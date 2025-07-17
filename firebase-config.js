cca888d8f2c06368dfd477df9eeb7b38e3fb0666
deepakkumar9575 <gindoripiplya@gmail.com>
2025-07-17 15:11:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,11 @@
-// firebase-config.js
-
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-
-// Your Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAIazwoh9hesNYpT5ywc-aX3qnj4UcGhKY",
   authDomain: "jeet365-fabf2.firebaseapp.com",
+  databaseURL: "https://jeet365-fabf2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "jeet365-fabf2",
-  storageBucket: "jeet365-fabf2.appspot.com",  // corrected
+  storageBucket: "jeet365-fabf2.firebasestorage.app",
   messagingSenderId: "862480702968",
   appId: "1:862480702968:web:fe41c67573c82cb7ec4a0d",
   measurementId: "G-LZRXW175NR"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-// Export for use in other files
-export { auth, db };
